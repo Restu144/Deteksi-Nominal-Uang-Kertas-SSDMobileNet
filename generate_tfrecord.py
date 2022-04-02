f65aa0713cdb5ed93b940266f51037576de1ae02
Restu144 <90734900+Restu144@users.noreply.github.com>
2022-04-02 16:55:13 +0700

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -75,8 +75,8 @@ def create_tf_example(group, path):
         xmaxs.append(row['xmax'] / width)
         ymins.append(row['ymin'] / height)
         ymaxs.append(row['ymax'] / height)
-        classes_text.append(row['class'].encode('utf8'))
-        classes.append(class_text_to_int(row['class']))
+        classes_text.append(str(row['class']).encode('utf8'))
+        classes.append(row['class'])
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
         'image/height': dataset_util.int64_feature(height),
